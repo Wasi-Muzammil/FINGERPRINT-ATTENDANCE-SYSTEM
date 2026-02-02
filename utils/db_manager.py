43f7e6b8a8e952794d58a57181ad414f65fb2404
Wasi-Muzammil <wasimuzammil2005@gmail.com>
2026-02-02 16:38:51 +0500

    Updated

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -37,7 +37,7 @@ class UserInformationDB(Base):
     slot_id = Column(PG_ARRAY(Integer), nullable=False)  # Array of slot IDs
     date = Column(String, nullable=False)  # DD/MM format
     time = Column(String, nullable=False)  # HH:MM:SS format
-    salary = Column(Numeric,nullable=True,default= Null)
+    salary = Column(Numeric(precision=10, scale=2),nullable=True,default= Null)
     created_at = Column(DateTime, default=datetime.now)
 
 class AttendanceRecordDB(Base):
